ed586182f93dbfab6e414df138f4d0f164edc11c
Milton Bezerra das Neves Neto <m.neto@academico.ifrn.edu.br>
2024-04-08 11:10:56 -0300

    projeto em 3d com delphi

---
+++ exemplo10/Unit1.pas
@@ -0,0 +1,26 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms3D, FMX.Types3D, FMX.Forms, FMX.Graphics, 
+  FMX.Dialogs, System.Math.Vectors, FMX.Controls3D, FMX.Objects3D;
+
+type
+  TForm1 = class(TForm3D)
+    Sphere1: TSphere;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
