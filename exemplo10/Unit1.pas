ea241bdc982d5152e16a859ea10ab3be687e8de8
Milton Bezerra das Neves Neto <m.neto@academico.ifrn.edu.br>
2024-04-08 11:41:59 -0300

    second commit

--- exemplo10/Unit1.pas
+++ exemplo10/Unit1.pas
@@ -5,11 +5,16 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms3D, FMX.Types3D, FMX.Forms, FMX.Graphics, 
-  FMX.Dialogs, System.Math.Vectors, FMX.Controls3D, FMX.Objects3D;
+  FMX.Dialogs, System.Math.Vectors, FMX.Controls3D, FMX.Objects3D, FMX.Ani,
+  FMX.MaterialSources;
 
 type
   TForm1 = class(TForm3D)
     Sphere1: TSphere;
+    LightMaterialSource1: TLightMaterialSource;
+    Light1: TLight;
+    FloatAnimation1: TFloatAnimation;
+    ColorAnimation1: TColorAnimation;
   private
     { Private declarations }
   public
